04459fecbd398e3b70010a0438af380b4f4ee3e8
Julian Haupt <julian.haupt@hauptmedia.de>
2015-11-02 19:15:53 +0100

    added ConsumerInterface

---
+++ src/Hauptmedia/Bundle/KafkaBundle/Consumer/ConsumerInterface.php
@@ -0,0 +1,13 @@
+<?php
+interface ConsumerInterface
+{
+    /**
+     * @param $topic Topic name
+     * @param $partition Partition
+     * @param $offset Message offset
+     * @param $payload Message payload
+     * @param $key Optional message key
+     * @return mixed
+     */
+    public function consume($topic, $partition, $offset, $payload, $key);
+}
