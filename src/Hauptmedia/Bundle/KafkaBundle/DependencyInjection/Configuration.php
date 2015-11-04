a44f5bda738e6f81752805d0a1871c33abf3accb
Julian Haupt <julian.haupt@hauptmedia.de>
2015-11-04 15:32:55 +0100

    added topic definition for kafka queue

--- src/Hauptmedia/Bundle/KafkaBundle/DependencyInjection/Configuration.php
+++ src/Hauptmedia/Bundle/KafkaBundle/DependencyInjection/Configuration.php
@@ -20,9 +20,22 @@ class Configuration implements ConfigurationInterface
         $treeBuilder = new TreeBuilder();
         $rootNode = $treeBuilder->root('hauptmedia_kafka');
 
-        // Here you should define the parameters that are allowed to
-        // configure your bundle. See the documentation linked above for
-        // more information on that topic.
+        $node = $rootNode
+            ->children()
+                ->arrayNode('topics')
+                    ->useAttributeAsKey('name')
+                    ->prototype('array')
+                        ->children()
+                            ->arrayNode('brokers')
+                            ->requiresAtLeastOneElement()
+                            ->prototype('scalar')
+                            ->end()
+                        ->end()
+                    ->end()
+                ->end()
+            ->end();
+
+
 
         return $treeBuilder;
     }
