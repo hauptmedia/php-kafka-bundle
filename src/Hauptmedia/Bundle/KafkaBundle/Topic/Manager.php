dbc1dbd7cc34f80e0f0017030a3fbafc477e8b50
Julian Haupt <julian.haupt@hauptmedia.de>
2015-11-05 15:50:17 +0100

    refactored Manager & Topic

--- src/Hauptmedia/Bundle/KafkaBundle/Topic/Manager.php
+++ src/Hauptmedia/Bundle/KafkaBundle/Topic/Manager.php
@@ -11,14 +11,37 @@ class Manager {
 
     public function addTopic($name, array $conf)
     {
-        $topic = new Topic();
-        $topic->setName($name);
+        $producer = new \RdKafka\Producer();
+        $producer->addBrokers(implode(',', $conf['brokers']));
 
-        $this->topics[] = $topic;
+        $producerTopicConf = new \RdKafka\TopicConf();
+        $producerTopic = $producer->newTopic($name, $producerTopicConf);
+
+        $consumer = new \RdKafka\Consumer();
+        $consumer->addBrokers(implode(',', $conf['brokers']));
+
+        $consumerTopicConf = new \RdKafka\TopicConf();
+        $consumerTopicConf->set("auto.commit.interval.ms", 1e3);
+        $consumerTopicConf->set("offset.store.sync.interval.ms", 60e3);
+        $consumerTopic = $consumer->newTopic($name, $consumerTopicConf);
+
+        $topic = new Topic($name, $producer, $producerTopic, $consumer, $consumerTopic);
+
+        $this->topics[$name] = $topic;
+    }
+
+    /**
+     * @param $name
+     * @return Topic
+     */
+    public function getTopic($name)
+    {
+        return array_key_exists($name, $this->topics) ?
+            $this->topics[$name] : null;
     }
 
     public function getTopics()
     {
-        return $this->topics;
+        return array_values($this->topics);
     }
 }
--- src/Hauptmedia/Bundle/KafkaBundle/Topic/Topic.php
+++ src/Hauptmedia/Bundle/KafkaBundle/Topic/Topic.php
@@ -1,16 +1,93 @@
 <?php
 namespace Hauptmedia\Bundle\KafkaBundle\Topic;
 
+use Hauptmedia\Bundle\KafkaBundle\Consumer\ConsumerInterface;
+
 class Topic {
+    const PARTITION_UA = RD_KAFKA_PARTITION_UA;
+
+    /**
+     * @var \RdKafka\Producer
+     */
+    protected $rdKafkaProducer;
+
+    /**
+     * @var \RdKafka\ProducerTopic
+     */
+    protected $rdKafkaProducerTopic;
+
+    /**
+     * @var \RdKafka\Consumer
+     */
+    protected $rdKafkaConsumer;
+
+    /**
+     * @var \RdKafka\ConsumerTopic
+     */
+    protected $rdKafkaConsumerTopic;
+
+    /**
+     * @var ConsumerInterface[]
+     */
+    protected $consumers = array();
+
     protected $name;
+
+    public function __construct(
+        $name,
+        \RdKafka\Producer $rdKafkaProducer = null,
+        \RdKafka\ProducerTopic $rdKafkaProducerTopic = null,
+        \RdKafka\Consumer $rdKafkaConsumer = null,
+        \RdKafka\ConsumerTopic $rdKafkaConsumerTopic = null
+    )
+    {
+        $this->name = $name;
+        $this->rdKafkaProducer = $rdKafkaProducer;
+        $this->rdKafkaProducerTopic = $rdKafkaProducerTopic;
+        $this->rdKafkaConsumer = $rdKafkaConsumer;
+        $this->rdKafkaConsumerTopic = $rdKafkaConsumerTopic;
+    }
 
     public function getName()
     {
         return $this->name;
     }
 
-    public function setName($name)
+    public function addConsumer(ConsumerInterface $consumer)
     {
-        $this->name = $name;
+        $this->consumers[] = $consumer;
+    }
+
+    /**
+     * Produce and send a single message to broker
+     * @param $payload is the message payload
+     * @param $key is an optional message key, if non-NULL it will be passed to the topic partitioner as well as be sent with the message to the broker and passed on to the consumer.
+     * @param int $partition is the target partition, either Topic::PARTITION_UA (unassigned) for automatic partitioning using the topic's partitioner function, or a fixed partition (0..N)
+     */
+    public function produce($payload, $key=null, $partition=Topic::PARTITION_UA)
+    {
+        $this->rdKafkaProducerTopic->produce($partition, 0, $payload, $key);
+    }
+
+    public function consume($partition=Topic::PARTITION_UA)
+    {
+        $partition=0;
+
+        if(!$this->consumers) {
+            throw new \Exception("Topic ".$this->name." has no registered consumers");
+        }
+
+        if(!$this->rdKafkaConsumerTopic) {
+            throw new \Exception("Topic ".$this->name." is not registered as consumer topic");
+        }
+
+        $this->rdKafkaConsumerTopic->consumeStart($partition, RD_KAFKA_OFFSET_BEGINNING);
+
+        while($message = $this->rdKafkaConsumerTopic->consume($partition, 10000)) {
+            foreach($this->consumers as $consumer) {
+                $consumer->consume($message->topic_name, $message->partition, $message->offset, $message->key, $message->payload);
+            }
+        }
+        $this->rdKafkaConsumerTopic->consumeStop($partition);
     }
 }
